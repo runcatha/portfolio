16cde38bee0e2b2c5b79849870360b7780cdf788
Cathleen Runde <cathleenrunde@new-host-4.home>
2021-11-01 19:08:09 -0700

    Navbar and rrd package

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo} from './NavbarElements'
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo>
+            dolla
+          </NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
