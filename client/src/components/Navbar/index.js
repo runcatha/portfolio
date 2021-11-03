33ff6f2773db8da3fbc3dcf2fa24d881a1a06816
Cathleen Runde <cathleenrunde@new-host-4.home>
2021-11-02 17:17:34 -0700

    deploy fix

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { FaBars } from 'react-icons/fa';
-import { IconContext } from 'react-icons/lib';
+// import { IconContext } from 'react-icons/lib';
 import { animateScroll as scroll } from 'react-scroll';
 import {
   Nav,
@@ -10,7 +10,7 @@ import {
   NavMenu,
   NavItem,
   NavLinks,
-  a
+  // a
 } from './NavbarElements'
 
 const Navbar = ({ toggle }) => {
